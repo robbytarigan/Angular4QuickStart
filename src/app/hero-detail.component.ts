18922645eb70a2dcc56add82d7f5c500058fdc41
Robby Tarigan <robbytarigan@hotmail.com>
2017-07-23 23:45:15 +0100

    Add stylesheet

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
@@ -7,7 +7,8 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
     constructor(
@@ -16,7 +17,7 @@ export class HeroDetailComponent implements OnInit {
         private location : Location
     ) { }
 
-    @Input() hero: Hero;
+    hero: Hero;
 
     goBack(): void {
         this.location.back();
